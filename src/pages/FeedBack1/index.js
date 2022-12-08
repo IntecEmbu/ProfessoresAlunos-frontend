664565657df85cda5152fe2e1fc665b891391f1e
Antonio Henrique <88168605+Henriquedacruz@users.noreply.github.com>
2022-12-08 11:58:26 -0300

    ListarMaterial

--- src/pages/FeedBack1/index.js
+++ src/pages/FeedBack1/index.js
@@ -4,27 +4,31 @@ import Button from 'react-bootstrap/Button';
 import Btn from '../../components/BotaoFlutuante';
 import Pesquisa from '../../components/Pesquisa/index.js';
 import ModalMaterialFeed from '../../components/ModalMaterialFeed/index.js';
+import Table from 'react-bootstrap/Table';
 import '../../styles/feedback1.css';
 import api2 from '../../config/configApi2.js'
+import axios from "axios";
 
 
 function Index() {
 
-    const baixar = async e => {
-      let d = document.createElement("iframe");
-      d.setAttribute("src", "http://localhost:3333/dowload")
-      document
-        .querySelector("#baixar")
-        .appendChild(d)
-        .setAttribute("style", "display: none");
-    }
-    const [loadModalAdd, setModalAdd] = useState(false);
+  const [gender, setGender] = useState([]);
 
-    function showAddModal() {
-        setModalAdd(true);
-        console.log('sssss')
-    }
-    const [image, setImage] = useState('');
+  const baixar = async e => {
+    let d = document.createElement("iframe");
+    d.setAttribute("src", "http://localhost:3333/dowload")
+    document
+      .querySelector("#baixar")
+      .appendChild(d)
+      .setAttribute("style", "display: none");
+  }
+  const [loadModalAdd, setModalAdd] = useState(false);
+
+  function showAddModal() {
+    setModalAdd(true);
+    console.log('sssss')
+  }
+  const [image, setImage] = useState('');
   const [status, setStatus] = useState({
     type: '',
     mensagem: ''
@@ -79,51 +83,86 @@ function Index() {
     getImages();
   }, []);
 
-    return (
-        <>
-            {loadModalAdd && <ModalMaterialFeed data-backdrop='static' isOpen={loadModalAdd} />}
+  useEffect(() => {
 
-            <Btn />
-            <div className='pai'>
-                <h1 className='IntecFeed'>INTEC FEEDBACK</h1>
-                <div className='containerFeed'>
-                    <header className='Form-header'>
-                        <div className='cont-pesquisa'>
-                            <Pesquisa className='pesquisa-avaliacao' />
-                            <Link to='/Feedback3'>
-                            {/* {sessionStorage.getItem("registro_numero") !== null ? :''}*/}
-                                <Button>Ver Feedbacks</Button>
-                            </Link> 
-                            <Button onClick={showAddModal} >Adicionar Material</Button>
-                        </div>
-                    </header>
-                    <body className='bodyForm1'>
-                        <div className='divTurmas'>
-                            {/* <p className='texto-divturma'>Professor</p>
+    async function getGender() {
+      const { data } = await axios.get(' http://localhost:8080/material ');
+      setGender(data);
+    }
+    getGender();
+  }, []);
+
+  return (
+    <>
+      {loadModalAdd && <ModalMaterialFeed data-backdrop='static' isOpen={loadModalAdd} />}
+
+      <Btn />
+      <div className='pai'>
+        <h1 className='IntecFeed'>INTEC FEEDBACK</h1>
+        <div className='containerFeed'>
+          <header className='Form-header'>
+            <div className='cont-pesquisa'>
+              <Pesquisa className='pesquisa-avaliacao' />
+              <Link to='/Feedback3'>
+                {/* {sessionStorage.getItem("registro_numero") !== null ? :''}*/}
+                <Button>Ver Feedbacks</Button>
+              </Link>
+              <Button onClick={showAddModal} >Adicionar Material</Button>
+            </div>
+          </header>
+          <body className='bodyForm1'>
+            {/* <div className='divTurmas'>
+                             <p className='texto-divturma'>Professor</p>
                             <p className='texto-divturma'>PW III</p>
-                            <p className='texto-divturma'>Matéria: Node.js</p> */}
+                            <p className='texto-divturma'>Matéria: Node.js</p>
                             <Link to='/Feedback2'>
                                 <Button className='btnAvancar'>Avançar</Button>
                             </Link>
-                        </div>
-                    </body>
-                    <div id="baixar">
-                      <button onClick={baixar}>Dowload</button>
-                    </div>
-                    <div className="img">
+                        </div> */}
+            <Table striped bordered hover >
+              <thead>
+                <tr>
+                  <th>id</th>
+                  <th>Material</th>
+                  <th>Ações</th>
+                </tr>
+              </thead>
+              <tbody >
+                {
+                  gender.map((gende) => (
+                    <tr key={gende.id_genero}>
+                      <td>{gende.id_genero}</td>
+                      <td>{gende.genero}</td>
+                      <td>
+                        <Link to="/Feedback3">
+                          <Button>Acessar</Button>
+                        </Link>
+                      </td>
 
-                                {data.map(image => (
-                                    <div key={image.id}>
-                                        <img src={url + image.image} alt={image.id} width="150" />
-                                        <hr />
-                                    </div>
-                                ))}
+                      {/* Botao de excluir */}
+                      <td>
+                        <Button
+                          variant="danger"
+                          onClick={baixar}
+                        >Baixar pdf
+                        </Button>
+                      </td>
+                    </tr>
+                  ))
+                }
+              </tbody>
+            </Table>
+          </body>
+          <div id="baixar">
 
-                            </div>
-                </div>
-            </div>
-        </>
-    )
+          </div>
+          <div className="img">
+
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default Index
